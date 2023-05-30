7993f8e95fa8ceee8546a56bc96296a9fe4d624e
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2023-05-30 11:38:21 +0200

    create create CRUD

---
+++ crud/create.js
@@ -0,0 +1,23 @@
+import { supabase } from "../lib/supabase";
+import { Alert } from "react-native";
+
+export default function create(table, datas, { session }) {
+  if (!session?.user) throw new Error("No user on the session!");
+
+  return supabase
+    .from(table)
+    .insert([{ datas, user_id: session?.user.id }]) /*  format props : [
+        { some_column: 'someValue', other_column: 'otherValue' },
+      ]  */
+    .eq("user_id")
+    .then(({ data, error, status }) => {
+      if (error && status !== 406) {
+        throw error;
+      }
+    })
+    .catch((error) => {
+      if (error instanceof Error) {
+        Alert.alert(error.message);
+      }
+    });
+}
