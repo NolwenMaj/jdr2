64a40b99e6e4395e47d854bfd263875776fafbe7
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2023-05-30 11:38:08 +0200

    create deleteData CRUD

---
+++ crud/deleteData.js
@@ -0,0 +1,23 @@
+
+import { supabase } from "../lib/supabase";
+import { Alert } from "react-native";
+
+export default function deleteData (table, datasToDelete, { session }) {
+  if (!session?.user) throw new Error("No user on the session!");
+
+  return supabase
+    .from(table)
+    .delete()
+    .eq("user_id", session?.user.id)    
+    .then(({ data, error, status }) => {
+      if (error && status !== 406) {
+        throw error;
+      }
+    })
+    .catch((error) => {
+      if (error instanceof Error) {
+        Alert.alert(error.message);
+      }
+    });
+}
+
